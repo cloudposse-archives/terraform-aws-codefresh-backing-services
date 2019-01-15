6990ce1f9ab42ded152cda514017de4eeb7f3d63
Josh Myers <joshuajmyers@gmail.com>
2019-01-15 17:33:36 +0000

    Add default global variables

--- main.tf
+++ main.tf
@@ -12,3 +12,42 @@ provider "aws" {
   }
 }
 
+# global variables
+#--------------------------------------------------------------
+variable "aws_assume_role_arn" {
+  type = "string"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Name  (e.g. `codefresh`)"
+  default     = "codefresh"
+}
+
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `eg` or `cp`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`Cluster`,`us-east-1.cloudposse.co`)"
+}
