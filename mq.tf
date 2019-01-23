a84289429d545397518f6333634e0d98896eaab8
Josh Myers <joshuajmyers@gmail.com>
2019-01-23 15:34:22 +0000

    Remove aws-mq-broker enabled flags
    Bump aws-mq-broker to 0.3.0, which does not feature enabled flags, so 
    remove them.
    
    In its current incarnation the terraform-aws-mq-broker does not support
    the enabled variable allowing for boolean creation of resources in the
    module, see [1] for more context.
    
    [1] https://github.com/cloudposse/terraform-aws-mq-broker/pull/4

--- mq.tf
+++ mq.tf
@@ -4,12 +4,6 @@ variable "mq_apply_immediately" {
   default     = "true"
 }
 
-variable "mq_enabled" {
-  type        = "string"
-  default     = ""
-  description = "Set to false to prevent the module from creating any resources"
-}
-
 variable "mq_auto_minor_version_upgrade" {
   type        = "string"
   description = "Enables automatic upgrades to new minor versions for brokers, as Apache releases the versions"
@@ -80,17 +74,15 @@ variable "mq_maintenance_time_zone" {
 # supply 2 subnets. Any more and the resource will complain. Similarly
 # you must pass a single subnet if running in SINGLE_INSTANCE mode
 locals {
-  mq_enabled    = "${var.mq_enabled != "" ? var.mq_enabled : var.enabled}"
   mq_subnet_ids = "${var.mq_deployment_mode == "ACTIVE_STANDBY_MULTI_AZ" ? slice(var.subnet_ids,0,2) : slice(var.subnet_ids,0,1)}"
 }
 
 module "amq" {
-  source                     = "git::https://github.com/cloudposse/terraform-aws-mq-broker.git?ref=tags/0.2.0"
+  source                     = "git::https://github.com/cloudposse/terraform-aws-mq-broker.git?ref=tags/0.3.0"
   namespace                  = "${var.namespace}"
   stage                      = "${var.stage}"
   name                       = "${var.name}"
   apply_immediately          = "${var.mq_apply_immediately}"
-  enabled                    = "${local.mq_enabled}"
   auto_minor_version_upgrade = "${var.mq_auto_minor_version_upgrade}"
   deployment_mode            = "${var.mq_deployment_mode}"
   engine_type                = "${var.mq_engine_type}"
@@ -109,51 +101,51 @@ module "amq" {
 }
 
 output "mq_broker_id" {
-  value       = "${local.mq_enabled ? module.amq.broker_id : ""}"
+  value       = "${module.amq.broker_id}"
   description = "AmazonMQ broker ID"
 }
 
 output "mq_broker_arn" {
-  value       = "${local.mq_enabled ? module.amq.broker_arn : ""}"
+  value       = "${module.amq.broker_arn}"
   description = "AmazonMQ broker ARN"
 }
 
 output "mq_primary_console_url" {
-  value       = "${local.mq_enabled ? module.amq.primary_console_url : ""}"
+  value       = "${module.amq.primary_console_url}"
   description = "AmazonMQ active web console URL"
 }
 
 output "mq_primary_ampq_ssl_endpoint" {
-  value       = "${local.mq_enabled ?  module.amq.primary_ampq_ssl_endpoint : ""}"
+  value       = "${module.amq.primary_ampq_ssl_endpoint}"
   description = "AmazonMQ primary AMQP+SSL endpoint"
 }
 
 output "mq_primary_ip_address" {
-  value       = "${local.mq_enabled ? module.amq.primary_ip_address : ""}"
+  value       = "${module.amq.primary_ip_address}"
   description = "AmazonMQ primary IP address"
 }
 
 output "mq_secondary_console_url" {
-  value       = "${local.mq_enabled ? module.amq.secondary_console_url : ""}"
+  value       = "${module.amq.secondary_console_url}"
   description = "AmazonMQ secondary web console URL"
 }
 
 output "mq_secondary_ampq_ssl_endpoint" {
-  value       = "${local.mq_enabled ? module.amq.secondary_ampq_ssl_endpoint : ""}"
+  value       = "${module.amq.secondary_ampq_ssl_endpoint}"
   description = "AmazonMQ secondary AMQP+SSL endpoint"
 }
 
 output "mq_secondary_ip_address" {
-  value       = "${local.mq_enabled ? module.amq.secondary_ip_address : ""}"
+  value       = "${module.amq.secondary_ip_address}"
   description = "AmazonMQ secondary IP address"
 }
 
 output "mq_admin_username" {
-  value       = "${local.mq_enabled ? module.amq.admin_username : ""}"
+  value       = "${module.amq.admin_username}"
   description = "AmazonMQ admin username"
 }
 
 output "mq_application_username" {
-  value       = "${local.mq_enabled ? module.amq.application_username : ""}"
+  value       = "${module.amq.application_username}"
   description = "AmazonMQ application username"
 }
